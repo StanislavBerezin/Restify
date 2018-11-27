3b753e3d9948ccec66aa3e6e716bab278ac0c296
Ubuntu <stanislav_berezin@hotmail.com>
2018-11-27 14:53:46 +1000

    register functionality for user

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const errors = require('restify-errors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const auth = require('../auth');
+const config = require('../config');
+
+module.exports = server => {
+  // Register User
+  server.post('/register', (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = new User({
+      email,
+      password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, async (err, hash) => {
+        // Hash Password
+        user.password = hash;
+        // Save User
+        try {
+          const newUser = await user.save();
+          res.send(201);
+          next();
+        } catch (err) {
+          return next(new errors.InternalError(err.message));
+        }
+      });
+    });
+  });
+
+};
