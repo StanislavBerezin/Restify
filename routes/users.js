45f93356a28e4e9facaffce3a8368543264b54be
Ubuntu <stanislav_berezin@hotmail.com>
2018-11-27 14:54:05 +1000

    authorise user

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,27 @@ module.exports = server => {
     });
   });
 
+  // Auth User
+  server.post('/auth', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      // Authenticate User
+      const user = await auth.authenticate(email, password);
+
+      // Create JWT
+      const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+        expiresIn: '15m'
+      });
+
+      const { iat, exp } = jwt.decode(token);
+      // Respond with token
+      res.send({ iat, exp, token });
+
+      next();
+    } catch (err) {
+      // User unauthorized
+      return next(new errors.UnauthorizedError(err));
+    }
+  });
 };
